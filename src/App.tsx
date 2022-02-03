fa905392a4cd89394732470a943e194c28b07708
Mathis Bullinger <mathis@bullinger.dev>
2022-02-03 09:47:25 +0100

    chore: initial minimal setup

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const App: React.FC = () => {
+  return <div>very interesting content</div>
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
+const IS_DEV = process.env.NODE_ENV === 'development'
+
+module.exports = {
+  mode: IS_DEV ? 'development' : 'production',
+  entry: {
+    app: path.join(__dirname, 'src/index.tsx'),
+  },
+  output: {
+    filename: '[name].js',
+    path: path.join(__dirname, 'build'),
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+  devServer: {
+    port: 3000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts)x?$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'index.html'),
+    }),
+  ],
+}
