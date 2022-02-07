7561a07ee715f8213da9e2ed1fc5778bc0f7ad2c
Mathis Bullinger <mathis@bullinger.dev>
2022-02-07 11:03:25 +0100

    fix: indicate liked photos on touch devices

--- src/screens/PageContainer.tsx
+++ src/screens/PageContainer.tsx
@@ -3,6 +3,13 @@ import { usePageIndex } from 'hooks/navigation'
 import Home from 'screens/Home'
 import Favorites from 'screens/Favorites'
 
+/* To enable the sliding transition between the home and favorites screens,
+   both screens are rendered inside this component and based on the current 
+   route the content is translated into view.
+   As an optimization, both screens technically only need to be rendered while 
+   the transition in in progress, but for this small example always rendering
+   both screens should be fine. */
+
 export default function PageContainer() {
   const page = usePageIndex()
 
